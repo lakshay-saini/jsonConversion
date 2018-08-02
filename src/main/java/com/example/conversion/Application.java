149ec966e64f4e7ab499749a1dd911b107fe6588
lakshay <lakshay.laky@gmail.com>
2018-08-03 01:59:03 +0530

    version-v0.1 : Getting url collection by stream

--- src/main/java/com/example/conversion/Application.java
+++ src/main/java/com/example/conversion/Application.java
@@ -4,24 +4,19 @@ import com.example.conversion.model.Json;
 import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 
-import java.util.HashSet;
 import java.util.LinkedHashMap;
-import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Application {
 
     public static void main(String[] args) {
         LinkedHashMap<String, JSONObject> list = new LinkedHashMap<>();
+
         JSONObject image = new JSONObject(Json.JSON_ONE).getJSONArray("getPropertyContent").getJSONObject(0).getJSONObject("images").getJSONObject("external");
         for (String key : image.keySet()) list.put(key, (JSONObject) image.get(key));
-        Set<String> urls = new HashSet<>();
-        for (Map.Entry<String, JSONObject> entry : list.entrySet()) {
-            String url = String.valueOf(entry.getValue().get("url"));
-            if(!StringUtils.isEmpty(String.valueOf(url))){
-                urls.add(url);
-            }
-        }
-        System.out.println("list = " + urls);
+        Set<String> urls = list.entrySet().stream().filter(value -> !StringUtils.isEmpty(String.valueOf(value.getValue().get("url")))).map(value -> String.valueOf(value.getValue().get("url"))).collect(Collectors.toSet());
+
+        System.out.println("urls = " + urls);
     }
 }
