b43cb32a5bbeb89f040c2e58265159ef05ae00b3
lakshay <lakshay.laky@gmail.com>
2018-08-03 03:37:44 +0530

    final commit

--- src/main/java/com/example/conversion/Application.java
+++ src/main/java/com/example/conversion/Application.java
@@ -11,11 +11,11 @@ import java.util.stream.Collectors;
 public class Application {
 
     public static void main(String[] args) {
-        LinkedHashMap<String, JSONObject> list = new LinkedHashMap<>();
+        LinkedHashMap<String, JSONObject> urlList = new LinkedHashMap<>();
 
-        JSONObject image = new JSONObject(Json.JSON_ONE).getJSONArray("getPropertyContent").getJSONObject(0).getJSONObject("images").getJSONObject("external");
-        for (String key : image.keySet()) list.put(key, (JSONObject) image.get(key));
-        Set<String> urls = list.entrySet().stream().filter(value -> !StringUtils.isEmpty(String.valueOf(value.getValue().get("url")))).map(value -> String.valueOf(value.getValue().get("url"))).collect(Collectors.toSet());
+        JSONObject images = new JSONObject(Json.JSON_ONE).getJSONArray("getPropertyContent").getJSONObject(0).getJSONObject("images").getJSONObject("external");
+        for (String key : images.keySet()) urlList.put(key, (JSONObject) images.get(key));
+        Set<String> urls = urlList.entrySet().stream().filter(value -> !StringUtils.isEmpty(String.valueOf(value.getValue().get("url")))).map(value -> String.valueOf(value.getValue().get("url"))).collect(Collectors.toSet());
 
         System.out.println("urls = " + urls);
     }
