74012c277173da67e5ff1e1f39da20d830ef7484
lakshay <lakshay.laky@gmail.com>
2018-08-03 00:51:50 +0530

    Getting images collection

--- src/main/java/com/example/conversion/Application.java
+++ src/main/java/com/example/conversion/Application.java
@@ -3,12 +3,12 @@ package com.example.conversion;
 import com.example.conversion.model.Json;
 import org.json.JSONObject;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Application {
 
     public static void main(String[] args) {
-        HashMap<String, String> list = new HashMap<>();
+        LinkedHashMap<String, String> list = new LinkedHashMap<>();
         JSONObject image = new JSONObject(Json.JSON_ONE).getJSONArray("getPropertyContent").getJSONObject(0).getJSONObject("images").getJSONObject("external");
         for (String key : image.keySet()) list.put(key, image.get(key).toString());
         System.out.println("list = " + list);
