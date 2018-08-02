2c1c63a9b5caf4f892e4c885d7ce2a343d1a4cc5
lakshay <lakshay.laky@gmail.com>
2018-08-03 01:49:22 +0530

    Getting url collection by conventional methods

--- src/main/java/com/example/conversion/Application.java
+++ src/main/java/com/example/conversion/Application.java
@@ -1,16 +1,27 @@
 package com.example.conversion;
 
 import com.example.conversion.model.Json;
+import org.apache.commons.lang3.StringUtils;
 import org.json.JSONObject;
 
+import java.util.HashSet;
 import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.Set;
 
 public class Application {
 
     public static void main(String[] args) {
-        LinkedHashMap<String, String> list = new LinkedHashMap<>();
+        LinkedHashMap<String, JSONObject> list = new LinkedHashMap<>();
         JSONObject image = new JSONObject(Json.JSON_ONE).getJSONArray("getPropertyContent").getJSONObject(0).getJSONObject("images").getJSONObject("external");
-        for (String key : image.keySet()) list.put(key, image.get(key).toString());
-        System.out.println("list = " + list);
+        for (String key : image.keySet()) list.put(key, (JSONObject) image.get(key));
+        Set<String> urls = new HashSet<>();
+        for (Map.Entry<String, JSONObject> entry : list.entrySet()) {
+            String url = String.valueOf(entry.getValue().get("url"));
+            if(!StringUtils.isEmpty(String.valueOf(url))){
+                urls.add(url);
+            }
+        }
+        System.out.println("list = " + urls);
     }
 }
